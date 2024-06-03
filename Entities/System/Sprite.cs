aec567f75536f3189110e3cc185a408ddf774628
Ycalk <118367882+Ycalk@users.noreply.github.com>
2024-06-03 14:01:12 +0500

    added combine textures method

--- Entities/System/Sprite.cs
+++ Entities/System/Sprite.cs
@@ -60,6 +60,29 @@ namespace Architecture.Entities.System
             return texture;
         }
 
+        public static Texture2D CombineTextures(GraphicsDevice graphicsDevice, Texture2D texture1, Texture2D texture2)
+        {
+            var width = Math.Max(texture1.Width, texture2.Width);
+            var height = Math.Max(texture1.Height, texture2.Height);
+            
+            var result = new RenderTarget2D(graphicsDevice, width, height);
+
+            graphicsDevice.SetRenderTarget(result);
+            graphicsDevice.Clear(Color.Transparent);
+
+            var spriteBatch = new SpriteBatch(graphicsDevice);
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(texture1, new Rectangle(0, 0, width, height), Color.White);
+
+            spriteBatch.Draw(texture2, new Rectangle(0, 0, width, height), Color.White);
+
+            spriteBatch.End();
+
+            graphicsDevice.SetRenderTarget(null);
+            return result;
+        }
+
         public void Draw(SpriteBatch spriteBatch, Vector2 positionInPixels) =>
             spriteBatch.Draw(Texture, new Rectangle((int)positionInPixels.X, (int)positionInPixels.Y, Width, Height), Color.White);
 
